c2343a6cf868bc8b1755252e0d514546012118c8
{ Chao } <charlzyx@qq.com>
2022-08-16 14:38:55 +0800

    Create livedata.ts

---
+++ livedata.ts
@@ -0,0 +1,224 @@
+import { Tracker, observable, observe, toJS } from '@formily/reactive';
+import { get as lodashGet, set as lodashSet } from 'lodash';
+import { useEffect, useReducer, useRef, useState } from 'react';
+
+const _ = { get: lodashGet, set: lodashSet };
+
+const factory = {
+  accId: 1,
+  cache: {} as any,
+  make: <S extends Record<string, any> | Array<any>>(path?: string, init?: S | (() => S)) => {
+    const parts = path?.split('.') || ['ROOT'];
+    const [namespace, others] = parts;
+    const cached = factory.cache[namespace];
+    if (cached) return cached;
+
+    const initData = typeof init === 'function' ? init() : init;
+
+    const ob = initData
+      ? others
+        ? observable(_.set({}, others, initData))
+        : observable(initData)
+      : others
+      ? observable(_.set({}, others, 'TO_BE_CONTINUTED'))
+      : observable({});
+
+    /** 直接 undfined 有问题 */
+    if (others && !initData) {
+      _.set(ob, others, undefined);
+    }
+
+    factory.cache[namespace] = ob;
+
+    if (namespace !== 'ROOT') {
+      factory.cache.ROOT = factory.cache.ROOT || observable({ ROOT: {} });
+      factory.cache.ROOT[namespace] = ob;
+    }
+
+    return ob;
+  },
+};
+
+const useId = () => {
+  const id = useRef(factory.accId++);
+  return id.current.toString();
+};
+
+// const opWrite = { set: true, delete: true, clear: true, add: true } as Record<string, boolean>;
+export function useLiveData<S extends Record<string, any> | Array<any> = Record<string, any>>(
+  path?: string,
+  init?: S | (() => S),
+): S & { raw: S };
+export function useLiveData<S extends Record<string, any> | Array<any> = Record<string, any>>(
+  init?: S | (() => S),
+): S & { raw: S };
+export function useLiveData<S extends Record<string, any> | Array<any> = Record<string, any>>(
+  path?: string,
+  initial?: S | (() => S),
+): S & { raw: S } {
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+  const id = useId();
+  const init = typeof path === 'string' ? initial : path;
+
+  const [namespace] = typeof path === 'string' ? path?.split('.') : [id];
+
+  const [data] = useState(factory.make(typeof path === 'string' ? path : id, init));
+
+  const [deps, setDeps] = useState<Record<string, true>>({ [namespace]: true });
+
+  /** 收集依赖 */
+  useEffect(() => {
+    const dispose = observe(data, (change) => {
+      const chain = change.path.join('.');
+      // 不是当前 namespace
+      const exist = deps[chain];
+      if (!exist) {
+        setDeps((old) => {
+          return {
+            ...old,
+            [chain.replace(`${namespace}.`, '')]: true,
+          };
+        });
+      }
+    });
+    return dispose;
+  }, [data, deps, id, namespace, path]);
+
+  useEffect(() => {
+    const walk = () => {
+      if (Object.keys(deps).length === 0) {
+        // 利用 toJS 的遍历, 访问到所有用到的节点
+        toJS(data);
+      } else {
+        Object.keys(deps).forEach((chain) => _.get(data, chain));
+      }
+      forceUpdate();
+    };
+    const tracker = new Tracker(() => {
+      tracker.track(walk);
+    });
+    tracker.track(walk);
+
+    return () => {
+      tracker.dispose();
+    };
+  }, [data, deps]);
+
+  return data;
+}
+export const set = (namespace: string, producer: (draft: any) => void, init?: any) => {
+  const ob = factory.make(namespace, init);
+  producer(ob);
+};
+import { Tracker, observable, observe, toJS } from '@formily/reactive';
+import { get as lodashGet, set as lodashSet } from 'lodash';
+import { useEffect, useReducer, useRef, useState } from 'react';
+
+const _ = { get: lodashGet, set: lodashSet };
+
+const factory = {
+  accId: 1,
+  cache: {} as any,
+  make: <S extends Record<string, any> | Array<any>>(path?: string, init?: S | (() => S)) => {
+    const parts = path?.split('.') || ['ROOT'];
+    const [namespace, others] = parts;
+    const cached = factory.cache[namespace];
+    if (cached) return cached;
+
+    const initData = typeof init === 'function' ? init() : init;
+
+    const ob = initData
+      ? others
+        ? observable(_.set({}, others, initData))
+        : observable(initData)
+      : others
+      ? observable(_.set({}, others, 'TO_BE_CONTINUTED'))
+      : observable({});
+
+    /** 直接 undfined 有问题 */
+    if (others && !initData) {
+      _.set(ob, others, undefined);
+    }
+
+    factory.cache[namespace] = ob;
+
+    if (namespace !== 'ROOT') {
+      factory.cache.ROOT = factory.cache.ROOT || observable({ ROOT: {} });
+      factory.cache.ROOT[namespace] = ob;
+    }
+
+    return ob;
+  },
+};
+
+const useId = () => {
+  const id = useRef(factory.accId++);
+  return id.current.toString();
+};
+
+// const opWrite = { set: true, delete: true, clear: true, add: true } as Record<string, boolean>;
+export function useLiveData<S extends Record<string, any> | Array<any> = Record<string, any>>(
+  path?: string,
+  init?: S | (() => S),
+): S & { raw: S };
+export function useLiveData<S extends Record<string, any> | Array<any> = Record<string, any>>(
+  init?: S | (() => S),
+): S & { raw: S };
+export function useLiveData<S extends Record<string, any> | Array<any> = Record<string, any>>(
+  path?: string,
+  initial?: S | (() => S),
+): S & { raw: S } {
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+  const id = useId();
+  const init = typeof path === 'string' ? initial : path;
+
+  const [namespace] = typeof path === 'string' ? path?.split('.') : [id];
+
+  const [data] = useState(factory.make(typeof path === 'string' ? path : id, init));
+
+  const [deps, setDeps] = useState<Record<string, true>>({ [namespace]: true });
+
+  /** 收集依赖 */
+  useEffect(() => {
+    const dispose = observe(data, (change) => {
+      const chain = change.path.join('.');
+      // 不是当前 namespace
+      const exist = deps[chain];
+      if (!exist) {
+        setDeps((old) => {
+          return {
+            ...old,
+            [chain.replace(`${namespace}.`, '')]: true,
+          };
+        });
+      }
+    });
+    return dispose;
+  }, [data, deps, id, namespace, path]);
+
+  useEffect(() => {
+    const walk = () => {
+      if (Object.keys(deps).length === 0) {
+        // 利用 toJS 的遍历, 访问到所有用到的节点
+        toJS(data);
+      } else {
+        Object.keys(deps).forEach((chain) => _.get(data, chain));
+      }
+      forceUpdate();
+    };
+    const tracker = new Tracker(() => {
+      tracker.track(walk);
+    });
+    tracker.track(walk);
+
+    return () => {
+      tracker.dispose();
+    };
+  }, [data, deps]);
+
+  return data;
+}
+export const set = (namespace: string, producer: (draft: any) => void, init?: any) => {
+  const ob = factory.make(namespace, init);
+  producer(ob);
+};
